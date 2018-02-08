c03fc41c0325d08e1bf15c052aa6266874dcdafb
davis <davis.f.peck@protonmail.com>
2018-02-07 18:43:19 -0600

    Restructured database schema to support multiple mat types

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -1,43 +1,63 @@
 CREATE TABLE ItemTypes (
-    itypeid INTEGER NOT NULL AUTO_INCREMENT, 
-    itypename VARCHAR, 
-    iscontainer BOOLEAN, 
-    PRIMARY KEY (itypeid)
+    itemtypeid IDENTITY,
+    itemtypename VARCHAR,
+    iscontainer BOOLEAN,
+    PRIMARY KEY (itemtypeid)
+    );
+CREATE TABLE MatTypes (
+    mattypeid VARCHAR NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (mattypeid)
     );
 CREATE TABLE EventTypes (
-    eventid INTEGER NOT NULL AUTO_INCREMENT, 
-    eventname VARCHAR NOT NULL, 
-    PRIMARY KEY (eventid)
+    eventtypeid BIGINT NOT NULL,   -- eventtypeid is not set automatically to
+    eventtypename VARCHAR NOT NULL, --   ensure that IDs are consistent across
+    PRIMARY KEY (eventtypeid)       --   platforms/implementations
     );
 CREATE TABLE Items (
-    itemid INTEGER NOT NULL AUTO_INCREMENT, 
-    itemtype INTEGER NOT NULL, 
-    PRIMARY KEY (itemid), 
-    FOREIGN KEY (itemtype) REFERENCES ItemTypes(itypeid)
+    itemid IDENTITY,
+    itemtype INTEGER NOT NULL,
+    PRIMARY KEY (itemid),
+    FOREIGN KEY (itemtype) REFERENCES ItemTypes(itemtypeid)
     );
 CREATE TABLE Mats (
-    matid INTEGER NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (matid)
+    matid IDENTITY,
+    mattype INTEGER NOT NULL,
+    matcomment VARCHAR,
+    PRIMARY KEY (matid),
+    FOREIGN KEY (mattype) REFERENCES MatTypes(mattypeid)
     );
 CREATE TABLE History (
-    item INTEGER NOT NULL, 
-    datetime TIMESTAMP NOT NULL, 
-    mat INTEGER NOT NULL, 
-    event INTEGER NOT NULL, 
-    sensor1 REAL, 
-    sensor2 REAL, 
-    sensor3 REAL, 
-    sensor4 REAL, 
-    -- x REAL NOT null, 
-    -- y REAL NOT null, 
-    CONSTRAINT eventinfo PRIMARY KEY (item, datetime), 
-    FOREIGN KEY (item) REFERENCES Items(itemid), 
-    FOREIGN KEY (event) REFERENCES EventTypes(eventid),
-    FOREIGN KEY (mat) REFERENCES Mats(matid)
+    item INTEGER NOT NULL,
+    datetime TIMESTAMP NOT NULL,
+    mat INTEGER NOT NULL,
+    eventtype INTEGER NOT NULL,
+    sensors ARRAY,
+    x REAL,
+    y REAL,
+    CONSTRAINT eventinfo PRIMARY KEY (item, datetime),
+    FOREIGN KEY (item) REFERENCES Items(itemid),
+    FOREIGN KEY (mat) REFERENCES Mats(matid),
+    FOREIGN KEY (eventtype) REFERENCES EventTypes(eventtypeid)
     );
-INSERT INTO EventTypes (eventname) VALUES ('ADDED');
-INSERT INTO EventTypes (eventname) VALUES ('REMOVED');
-INSERT INTO EventTypes (eventname) VALUES ('REPLACED');
-INSERT INTO EventTypes (eventname) VALUES ('REDUCED');
-INSERT INTO EventTypes (eventname) VALUES ('REFILLED');
-INSERT INTO EventTypes (eventname) VALUES ('SLID');
+
+-----------------------
+-- EVENTTYPE ENTRIES --
+-----------------------
+-- brand new item added to a mat
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (0, 'ADDED');
+
+-- item completely removed from a mat
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (1, 'REMOVED');
+
+-- item that was removed from a mat placed on a mat (may be the same mat or a
+-- different mat
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (2, 'REPLACED');
+
+-- container reduced in weight
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (3, 'REDUCED');
+
+-- container increased in weight
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (4, 'REFILLED');
+
+-- item slid across a mat (NOT IMPLEMENTED)
+--INSERT INTO EventTypes (eventname) VALUES ('SLID');
