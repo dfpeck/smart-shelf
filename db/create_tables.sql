f4696543a0cfe6af25267c1c156dd427c9a50e13
davis <davis.f.peck@protonmail.com>
2018-02-16 16:52:03 -0600

    Added table constraints to disallow 0 and empty string as primary keys

--- db/create_tables.sql
+++ db/create_tables.sql
@@ -4,28 +4,33 @@ CREATE TABLE ItemTypes (
     iscontainer BOOLEAN NOT NULL DEFAULT FALSE,
     itemtypecomment VARCHAR,
     PRIMARY KEY (itemtypeid),
+    CHECK (itemtypeid != 0)
     );
 CREATE TABLE Items (
     itemid IDENTITY,
     itemtype BIGINT NOT NULL,
     PRIMARY KEY (itemid),
-    FOREIGN KEY (itemtype) REFERENCES ItemTypes(itemtypeid)
+    FOREIGN KEY (itemtype) REFERENCES ItemTypes(itemtypeid),
+    CHECK (itemid != 0)
     );
 CREATE TABLE MatTypes (
     mattypeid VARCHAR NOT NULL AUTO_INCREMENT,
-    PRIMARY KEY (mattypeid)
+    PRIMARY KEY (mattypeid),
+    CHECK (mattypeid != '')
     );
 CREATE TABLE Mats (
     matid IDENTITY,
     mattype VARCHAR NOT NULL,
     matcomment VARCHAR,
     PRIMARY KEY (matid),
-    FOREIGN KEY (mattype) REFERENCES MatTypes(mattypeid)
+    FOREIGN KEY (mattype) REFERENCES MatTypes(mattypeid),
+    CHECK (matid != 0)
     );
 CREATE TABLE EventTypes (
     eventtypeid BIGINT NOT NULL,    -- eventtypeid is not set automatically to
     eventtypename VARCHAR NOT NULL, --   ensure that IDs are consistent across
-    PRIMARY KEY (eventtypeid)       --   platforms/implementations
+    PRIMARY KEY (eventtypeid),      --   platforms/implementations
+    CHECK (eventtypeid != 0)
     );
 CREATE TABLE History (
     item BIGINT NOT NULL,
@@ -45,20 +50,20 @@ CREATE TABLE History (
 -- EVENTTYPE ENTRIES --
 -----------------------
 -- brand new item added to a mat
-INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (0, 'ADDED');
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (1, 'ADDED');
 
 -- item completely removed from a mat
-INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (1, 'REMOVED');
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (2, 'REMOVED');
 
 -- item that was removed from a mat placed on a mat (may be the same mat or a
 -- different mat
-INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (2, 'REPLACED');
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (3, 'REPLACED');
 
 -- container reduced in weight
-INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (3, 'REDUCED');
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (4, 'REDUCED');
 
 -- container increased in weight
-INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (4, 'REFILLED');
+INSERT INTO EventTypes (eventtypeid, eventtypename) VALUES (5, 'REFILLED');
 
 -- item slid across a mat (NOT IMPLEMENTED)
 --INSERT INTO EventTypes (eventname) VALUES ('SLID');
