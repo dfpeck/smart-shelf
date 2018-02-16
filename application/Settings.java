5fb66a7a39c1d97a6a19eb46739c6e42ff37ba14
shy1625 <33158850+shy1625@users.noreply.github.com>
2018-02-16 13:56:29 -0600

    Add files via upload

---
+++ application/Settings.java
@@ -0,0 +1,17 @@
+package com.example.shy16.smartshelf;
+
+import android.os.Bundle;
+
+/**
+ * Created by shy16 on 2/16/2018.
+ */
+
+class Settings extends List{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.popup);
+    }
+}
