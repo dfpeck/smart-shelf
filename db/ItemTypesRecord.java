84bb787dec559302ef49ec70a86eedf32ad725f2
davis <davis.f.peck@protonmail.com>
2018-02-16 17:26:24 -0600

    Removed unnecessary code

--- db/ItemTypesRecord.java
+++ db/ItemTypesRecord.java
@@ -1,7 +1,6 @@
 package db;
 
 import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 import java.sql.SQLException;
 
---
+++ db/MatTypesRecord.java
@@ -0,0 +1,10 @@
+package db;
+
+import java.sql.PreparedStatement;
+
+import java.sql.SQLException;
+
+public class MatTypesRecord extends TableRecord {
+    /* INSERTION METHODS */
+    public static String insert (Db db,
+                                 String
