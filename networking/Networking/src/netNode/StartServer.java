abaf110e038da0094109484974d4626fb5f3b4a3
Taylor Hoss <hosstay@gmail.com>
2018-04-11 15:01:22 -0500

    re-added getIpAddress since it's needed to know server IP to give to other components.

--- networking/Networking/src/netNode/StartServer.java
+++ networking/Networking/src/netNode/StartServer.java
@@ -8,8 +8,12 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.util.Enumeration;
 
 
 public class StartServer {
@@ -216,5 +220,46 @@ public class StartServer {
             System.out.println("IOException in dumpDatabase");
     	}
     }
+    
+    // This finds the IP address that the socket is hosted on, so the server's IP/port
+    // For all network interfaces and all IPs connected to said interfaces print
+    // those that are site local addresses (an address which doesn't have the
+    // global prefix and thus is only on this network).
+    public String getIpAddress() {
+        String ip = "";
+        try {
+            // Enumaration consisting of all interfaces on this machine
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+
+                // For the specific networkInterface create an enumeration
+                // consisting of all IP addresses on said interface
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+
+                // Rotate through all IP addresses on the networkInterface and
+                // print the IP if the IP is a SiteLocalAddress.
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress
+                            .nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "Server running at : "
+                                + inetAddress.getHostAddress();
+                    }
+                }
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "IOException in getIpAddress" + e.toString() + "\n";
+        }
+        return ip;
+    }  
 }
 
