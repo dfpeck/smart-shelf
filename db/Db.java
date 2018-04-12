f469db19fb17ef5e7b6b72f4f98bf93fe23cf917
davis <davis.f.peck@protonmail.com>
2018-04-11 22:11:40 -0500

    Revert "Added getFile to Db"
    This reverts commit 7a883af83bcffae8a134900f60c5de8dea041eb3.

--- db/Db.java
+++ db/Db.java
@@ -77,13 +77,6 @@ public class Db {
 
     /* UPDATE METHODS */
 
-
-    /* ACCESSORS */
-    public File getFile () {
-        return file;
-    }
-
-
     /* HELPER FUNCTIONS */
     public static Vector<String> readSqlFromFile (String sqlFileName) {
         Scanner sqlScanner;
