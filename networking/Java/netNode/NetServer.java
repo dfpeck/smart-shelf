c7dd1b6a1fd865fda8e21a447ac72b2114cc73c9
hosstay <hosstay@gmail.com>
2018-04-24 19:48:47 -0500

    final touches on db object sending.

--- networking/Java/netNode/NetServer.java
+++ networking/Java/netNode/NetServer.java
@@ -6,7 +6,7 @@ import java.io.OutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
-import netNode.TEST_NetServer;
+import Test.TEST_NetServer;
 
 
 public class NetServer extends Thread {
