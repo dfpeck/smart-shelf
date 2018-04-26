7b133b7a0f3e8afa4decf85a3d4e7250b56a5280
anguyen <shy1625@yahoo.com>
2018-04-25 19:03:53 -0500

    Added some of the networking code

--- application/MainActivity.java
+++ application/MainActivity.java
@@ -7,6 +7,7 @@ import android.widget.ExpandableListView;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import netNode.NetUI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +19,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Start of network code
+        String ip;
+        String str;
+
+        NetUI netUI = new NetUI(ip);
+        netUI.start();
+
+        // When want a record
+        netUI.SendString("record 1");
+
+        //split the string here
+
+        Boolean flag = false;
+        do {
+            str = netUI.pop();
+            if (str[0] == "record") {
+                weight[str[1]] = str[2];
+                text[str[1]] = str[3];
+                flag = true;
+            }
+            else if (str[0] = "add") {
+                //add new item
+            }
+        } while (flag == false);
+
+        //End of network code
 
         listView = (ExpandableListView)findViewById(R.id.lvExp);
         initData();
