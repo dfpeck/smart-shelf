f831ab6c8e8669127ae228e4fd8abe32f760cd4e
Parker <parkermattson@gmail.com>
2018-05-02 19:32:50 -0500

    added main class to example code
    added a main class to the example code

--- mat/RPi/TEST_pi4j_example.java
+++ mat/RPi/TEST_pi4j_example.java
@@ -5,55 +5,66 @@ import com.pi4j.util.Console;
 import java.io.IOException;
 import java.util.Date;
 
-public static void main(String args[]) throws InterruptedException, IOException {
-	final Console console = new Console();
-	
-	console.rirle("Arduino to Pi Test", "Test 1");
-	
-	console.promtForExit();
-	
-	final Serial serial = SerialFactory.createInstance();
-	
-	serial.addListener(new SerialDataEventListener() {
-		@Override
-		public void dataReceived(SerialDataEvent event) {
+public class TEST_pi4j_example {
+	public static void main(String args[]) throws InterruptedException, IOException {
+		final Console console = new Console();
+		
+		console.rirle("Arduino to Pi Test", "Test 1");
+		
+		console.promtForExit();
+		
+		final Serial serial = SerialFactory.createInstance();
+		
+		serial.addListener(new SerialDataEventListener() {
+			@Override
+			public void dataReceived(SerialDataEvent event) {
 
-			// NOTE! - It is extremely important to read the data received from the
-			// serial port.  If it does not get read from the receive buffer, the
-			// buffer will continue to grow and consume memory.
+				// NOTE! - It is extremely important to read the data received from the
+				// serial port.  If it does not get read from the receive buffer, the
+				// buffer will continue to grow and consume memory.
 
-			// print out the data received to the console
-			try {
-				console.println("[ASCII DATA] " + event.getAsciiString());
-			} catch (IOException e) {
-				e.printStackTrace();
+				// print out the data received to the console
+				try {
+					console.println("[ASCII DATA] " + event.getAsciiString());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
-		}
-	});
-	
-	try {
-		SerialConfig config = new SerialConfig();
+		});
 		
-		config.device(SerialPort.getDefaultPort())
-			.baud(Baud._38400)
-			.dataBits(DataBits._8)
-			.parity(Parity.NONE)
-			.stopBits(StopBits._1)
-			.flowControl(FlowControl.NONE);
+		try {
+			SerialConfig config = new SerialConfig();
 			
-		if (args.length > 0){
-			config = CommandArgumentParser.getSerialConfig(config, args);
-	}
+			config.device(SerialPort.getDefaultPort())
+				.baud(Baud._38400)
+				.dataBits(DataBits._8)
+				.parity(Parity.NONE)
+				.stopBits(StopBits._1)
+				.flowControl(FlowControl.NONE);
+				
+			if (args.length > 0){
+				config = CommandArgumentParser.getSerialConfig(config, args);
+		}
+			
+		console.box(" Connecting to: " + config.toString(),
+				"Recieving data every 1 second.");
+			
 		
-	console.box(" Connecting to: " + config.toString(),
-			"Recieving data every 1 second.");
-		
-	
-	try {serial.open(config);}
-	catch (IO ioe) {
-		throw new RuntimeException(ioe);
-	}
-		
-		
-	
+		try {serial.open(config);}
+		catch (IO ioe) {
+			throw new RuntimeException(ioe);
+		}
+			
+		Thread me = Thread.currentThread();
+		  synchronized (me)
+		  {
+			me.wait();
+		  }
+		}
+		catch (SerialPortException ex)
+		{
+		  System.out.println(" ==>> SERIAL SETUP FAILED : " + ex.getMessage());
+		  return;
+		}
+  }
 }
